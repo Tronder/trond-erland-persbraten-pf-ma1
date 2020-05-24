b2a690a19243277693873e286e6cd830fb1ffdab
Tronder <63967146+Tronder@users.noreply.github.com>
2020-05-24 21:24:21 +0200

    Added module assignment 1 js file
    Added module assignment 1 for programming foundation

---
+++ script.js
@@ -0,0 +1,37 @@
+// question 1
+
+var firstVar;
+
+// question 2
+
+var name = "Trond";
+
+// question 3
+
+var myNumber = 20;
+
+// question 4
+
+var division = 20 / 5;
+
+// question 5
+
+typeof "frog";
+
+// question 6
+
+var orderHasShipped = false;
+
+// question 7
+
+if (orderHasShipped === true) {
+console.log("true");
+} else {
+console.log("false");
+}
+
+// question 8
+
+for (var i = 0; i < 10; i++) {
+console.log(i);
+}
